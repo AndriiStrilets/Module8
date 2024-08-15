6a7b3f5bccc4ac785b72ce2429ec762eb8a0c131
Andrii_Strilets <strelets.and@protonmail.com>
2024-08-15 21:32:30 +0200

    Module 8 Homework

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        circle.printName();
+
+        Shape quad = new Quad();
+        quad.printName();
+        
+        Shape rectangle = new Rectangle();
+        rectangle.printName();
+
+        Shape square = new Square();
+        square.printName();
+
+        Shape triangle = new Triangle();
+        triangle.printName();
+
+    }
+}
---
+++ Shape.java
@@ -0,0 +1,35 @@
+public class Shape {
+
+    public void printName(){
+    }
+}
+
+class Rectangle extends Shape{
+    public void printName(){
+        System.out.println("Rectangle");
+    }
+}
+
+class Circle extends Shape{
+    public void printName(){
+        System.out.println("Circle");
+    }
+}
+
+class Triangle extends Shape{
+    public void printName(){
+        System.out.println("Triangle");
+    }
+}
+
+class Quad extends Shape{
+    public void printName(){
+        System.out.println("Quad");
+    }
+}
+
+class Square extends Shape{
+    public void printName(){
+        System.out.println("Square");
+    }
+}
